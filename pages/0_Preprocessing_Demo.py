20c8597a9b75b36bb77102df5fc7cbe5efa6997a
Julian Kaljuvee <kaljuvee@gmail.com>
2023-09-08 13:13:03 +0300

    Update and rename Preprocessing.py to 0_Preprocessing_Demo.py

--- pages/0_Preprocessing_Demo.py
+++ pages/0_Preprocessing_Demo.py
@@ -3,6 +3,7 @@ from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 import pytesseract
+import numpy as np
 
 # Function to read an image from an uploaded file
 def read_image(uploaded_file):
